adae44e98c7887cbc34a663f4ab520a67370f70b
JonPro0 <zellerhoff05@gmail.com>
2023-02-02 16:17:03 +0100

    USerinformation

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,31 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { Client, OpenAPI } from "@lets-science/letsscience-client";
+import { Client, OpenAPI, User } from "@lets-science/letsscience-client";
 import { useClient } from "../hooks/useClient";
 
 function Profile() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState<User|null>(null);
   let client = useClient();
 
   useEffect(() => {
-    const signup = async () => {
+    const getUser = async () => {
       client.user
         .getApiUserSelf()
         .then((resp) => {
           setUser(resp);
+          
         })
         .catch((e) => {
           console.log(e);
         });
     };
-    signup();
+    getUser();
 
     return () => {};
   }, []);
-  console.log(user);
+
+  if(user === null) {
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
 
   return (
     <React.Fragment>
-      <h1>Your Site</h1>
+      <h1>{user.name} Site</h1>
       <br />
     </React.Fragment>
   );
