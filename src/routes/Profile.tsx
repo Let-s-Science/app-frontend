536de450cd3625f2225f67b10076acf53cb7d720
Marius Krüger <suiramx11@icloud.com>
2023-02-02 12:19:47 +0100

    profile api request

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Client } from "@lets-science/letsscience-client";
 
 function Profile() {
-  return <div>Profile</div>;
+  const [user, setUser] = useState({});
+  let client = new Client({
+    BASE: import.meta.env.VITE_BACKEND_URL,
+    WITH_CREDENTIALS: true,
+  });
+  useEffect(() => {
+    const signup = async () => {
+      client.user
+        .postApiUserSelf()
+        .then((resp) => {
+          setUser(resp);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
+    signup();
+
+    return () => {};
+  }, []);
+  console.log(user);
+
+  return (
+    <React.Fragment>
+      <h1>Your Site</h1>
+      <br />
+    </React.Fragment>
+  );
 }
 
 export default Profile;
