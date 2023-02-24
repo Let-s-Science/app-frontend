7eb7f0d4250217a3fcf01baf446a3f435432e06e
Marius Krüger <suiramx11@icloud.com>
2023-02-24 10:01:59 +0100

    Startseite aktualisiert

--- src/routes/Start.tsx
+++ src/routes/Start.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { User } from "@lets-science/letsscience-client";
+import React, { useEffect, useState } from "react";
+import { useClient } from "../hooks/useClient";
 
 function Start() {
-  return <div>Start</div>;
+  const [user, setUser] = useState<User | null>(null);
+  let client = useClient();
+
+  useEffect(() => {
+    const getUser = async () => {
+      client.user
+        .getApiUserSelf()
+        .then((resp) => {
+          setUser(resp);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    };
+    getUser();
+
+    return () => {};
+  }, []);
+
+  if (user === null) {
+    return <React.Fragment>Couldn't load data</React.Fragment>;
+  }
+  return (
+    <React.Fragment>
+      <h1>Welcome back, {user.name}</h1>
+      <h2>Your Challenges</h2>
+      {/* <br></br> */}
+      <h2>Your Quiz</h2>
+    </React.Fragment>
+  );
 }
 
 export default Start;
