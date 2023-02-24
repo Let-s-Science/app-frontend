06d1287d37bc1eb6d6ba1056b6659fd92bb45aae
FinnRG <finnagertner@protonmail.com>
2023-02-24 14:41:44 +0100

    fix: Non transparent footer

--- src/routes/Challenges.tsx
+++ src/routes/Challenges.tsx
@@ -5,15 +5,17 @@ import { useClient } from "../hooks/useClient";
 import ChallengeProgressCard from "../components/ChallengeProgressCard";
 import { Accordion, Grid } from "@mantine/core";
 import colorMap from "../utils/colorMap";
+import { useListState } from "@mantine/hooks";
 
 const getCategories = (challenges: Challenge[]) => {
-  return [...new Set(challenges.flatMap((ch) => ch.category))];
+  const l = [...new Set(challenges.flatMap((ch) => ch.category))];
+  return l;
 };
 
 function Challenges() {
   const client = useClient();
 
-  const [value, setValue] = useState(["Water"]);
+  const [value, valueHandlers] = useListState<string>([]);
   const [challenges, setChallenges] = useState<Challenge[]>([]);
   const [userChallenges, setUserChallenges] = useState<UserChallenge[]>([]);
   const colors = colorMap(challenges);
@@ -22,47 +24,53 @@ function Challenges() {
   useEffect(() => {
     client.challenge.getApiChallenges().then((resp) => {
       setChallenges(resp);
-      setValue(getCategories(resp));
+      valueHandlers.append(...getCategories(resp));
     });
-    client.challenge
-      .getApiChallengesSelf()
-      .then((resp) => setUserChallenges(resp));
+    client.challenge.getApiChallengesSelf().then((resp) => {
+      setUserChallenges(resp);
+      valueHandlers.append("My Challenges");
+    });
   }, []);
 
   const userChallengeList = userChallenges.map((userChallenge, key) => {
     const challenge = challenges.find(
       (x) => x.id === userChallenge.challenge_id
-    )!;
-    <Grid.Col span={6}>
-      <ChallengeProgressCard
-        key={key}
-        chall={challenge}
-        user_chall={userChallenge}
-      />
-    </Grid.Col>;
+    );
+    if (!challenge) {
+      return;
+    }
+    return (
+      <Grid.Col span={6} key={key}>
+        <ChallengeProgressCard chall={challenge} user_chall={userChallenge} />
+      </Grid.Col>
+    );
   });
 
   return (
-    <>
-      <Accordion multiple value={value} onChange={setValue}>
-        {categories.map((category, index) => (
-          <Accordion.Item value={category} key={index}>
-            <Accordion.Control>{category}</Accordion.Control>
-            <Accordion.Panel>
-              <Grid>
-                {challenges
-                  .filter((x) => x.category === category)
-                  .map((ch, key) => (
-                    <Grid.Col span={6} key={key}>
-                      <ChallengeCard challenge={ch} colorMap={colors} />
-                    </Grid.Col>
-                  ))}
-              </Grid>
-            </Accordion.Panel>
-          </Accordion.Item>
-        ))}
-      </Accordion>
-    </>
+    <Accordion multiple value={value} onChange={valueHandlers.setState}>
+      <Accordion.Item value="My Challenges">
+        <Accordion.Control>{"My Challenges"}</Accordion.Control>
+        <Accordion.Panel>
+          <Grid>{userChallengeList}</Grid>
+        </Accordion.Panel>
+      </Accordion.Item>
+      {categories.map((category, index) => (
+        <Accordion.Item value={category} key={index}>
+          <Accordion.Control>{category}</Accordion.Control>
+          <Accordion.Panel>
+            <Grid>
+              {challenges
+                .filter((x) => x.category === category)
+                .map((ch, key) => (
+                  <Grid.Col span={6} key={key}>
+                    <ChallengeCard challenge={ch} colorMap={colors} />
+                  </Grid.Col>
+                ))}
+            </Grid>
+          </Accordion.Panel>
+        </Accordion.Item>
+      ))}
+    </Accordion>
   );
 }
 
