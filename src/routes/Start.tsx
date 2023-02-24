c7c5309fd3084a80e8c9c6cc167901e9faf4b2c6
Marius Krüger <suiramx11@icloud.com>
2023-02-24 11:32:05 +0100

    Startseite mit Score und Level

--- src/routes/Start.tsx
+++ src/routes/Start.tsx
@@ -1,6 +1,9 @@
 import { User } from "@lets-science/letsscience-client";
 import React, { useEffect, useState } from "react";
+import { StatsRing } from "../components/StatsRing";
 import { useClient } from "../hooks/useClient";
+import { berechneFortschritt, berechneLevel } from "../util";
+import { Box, Card, Title } from "@mantine/core";
 
 function Start() {
   const [user, setUser] = useState<User | null>(null);
@@ -28,9 +31,29 @@ function Start() {
   return (
     <React.Fragment>
       <h1>Welcome back, {user.name}</h1>
+      <Card shadow="sm" p="lg" radius="md" withBorder>
+        <Card.Section>
+          <br />
+          <Title>{user.score}</Title>
+          <h3 style={{ textAlign: "center" }}>Your current Score!</h3>
+        </Card.Section>
+      </Card>
+      <StatsRing
+        data={[
+          {
+            label: "You are currently Level",
+            stats: berechneLevel(user.score),
+            progress: berechneFortschritt(user.score),
+            color: "green",
+            icon: "up",
+          },
+        ]}
+      />
       <h2>Your Challenges</h2>
       {/* <br></br> */}
       <h2>Your Quiz</h2>
+
+      <h2>Contact us!</h2>
     </React.Fragment>
   );
 }
